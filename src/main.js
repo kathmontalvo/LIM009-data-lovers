2af065763f705aacd9326d724b70a47f0b110e8f
kathmontalvo <kathmontalvo123@gmail.com>
2019-04-10 08:44:12 -0500

    añadiendo gráfico por campeón, de su informacion general

--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ const search = document.getElementById('search');
 const btnHome = document.getElementById('btn-home');
 const btnChamps = document.getElementById('btn-champs');
 const btnStats = document.getElementById('btn-stats')
+const infoChampionPage = document.getElementById('info-champions-pg');
+
 // Elementos donde se imprimirá info de templates
 const championsListElement = document.getElementById('champions');
 const infoChampion = document.getElementById('info-champions');
@@ -55,23 +57,20 @@ const printMainInfo = (div) => {
     fetch('data/lol/lol.json')
       .then(res => res.json())
       .then(championsData => {
+        console.log(championsData)
         const listOfChampions = Object.entries(championsData.data);
         listOfChampions.filter((obj) => {
           if (atribId === obj[1].id) {
+            console.log(Object.values(obj[1].info))
             const string = `
       <figure class="champ-img">
       <img class="champion-info-img"src=${obj[1].splash} alt=${obj[1].name}>
       </figure>
       <section class="info-gnrl">
       <div class="champion-info-name">${obj[1].name}</div>
+      <div class="champion-info-title">"${obj[1].title}"</div>
       <div class="champion-info-rol">Rol: ${obj[1].tags}</div>
-      <div class="champ-info-">
-      <ul class="info-general">
-      <li>AD-attack: ${obj[1].info.attack}</li>
-      <li>AP-magic: ${obj[1].info.magic}</li>
-      <li>DEF-defense: ${obj[1].info.defense}</li>
-      <li>DIF-difficulty: ${obj[1].info.difficulty}</li>
-      </ul>
+      <div id="champ-info" class="champ-info-">
       </section>
       <section class="info-stats">
       <div class="champ-descrip">Description:${obj[1].blurb}</div>
@@ -79,18 +78,26 @@ const printMainInfo = (div) => {
       <table class="table-stat">
       <tr>
         <th>HP</th>
-        <th>MP</th>
-        <th>Armor</th>
-        <th>AD</th>
-        <th>HP reg</th>
-        <th>MP reg</th>
-      </tr>
-      <tr>
         <td>${obj[1].stats.hp} (+${obj[1].stats.hpperlevel} por nivel) </td>
+        </tr>
+        <tr>
+        <th>MP</th>
         <td>${obj[1].stats.mp} (+${obj[1].stats.mpperlevel} por nivel)</td>
+        </tr>
+        <tr>
+        <th>Armor</th>
         <td>${obj[1].stats.armor} (+${obj[1].stats.armorperlevel} por nivel)</td>
+        </tr>
+        <tr>
+        <th>AD</th>
         <td>${obj[1].stats.attackdamage} (+${obj[1].stats.attackdamageperlevel} por nivel)</td>
+        </tr>
+      <tr>
+        <th>HP reg</th>
         <td>${obj[1].stats.hpregen} (+${obj[1].stats.hpregenperlevel} por nivel)</td>
+        </tr>
+      <tr>
+        <th>MP reg</th>
         <td>${obj[1].stats.mpregen} (+${obj[1].stats.mpregenperlevel} por nivel) </td>
       </tr>
       </table>
@@ -100,11 +107,12 @@ const printMainInfo = (div) => {
             divInfo.className = 'data-champ';
             divInfo.id = 'data-champ';
             infoChampion.appendChild(divInfo);
+            statInfoChart(obj);
           }
         });
       });
     championsListElement.classList.add('hide');
-    infoChampion.classList.remove('hide');
+    infoChampionPage.classList.remove('hide');
     navFilterAndSearch.classList.add('hide');
     asideStats.classList.add('hide');
   });
@@ -117,7 +125,33 @@ const statOfChamps = (arr) => {
   maxAd.innerHTML = lol.statOfChampions(arr, 'ad', 'max');
 };
 
-
+const statInfoChart = obj => {
+  const string = `<canvas id="chart-top" class="chart">`
+  document.getElementById('champ-info').innerHTML = string;
+  const ctx = document.getElementById('chart-top').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: Object.keys(obj[1].info),
+      datasets: [{
+        label: 'my dataset',
+        data: Object.values(obj[1].info),
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+      }]
+    },
+    options: {
+      scale: {
+        ticks: {
+          max: 10,
+          min: 0,
+          stepSize: 2
+        }
+      }
+    }
+  })
+}
 const statChart = arr => {
   const string = `<canvas id="chart-top" class="chart">`
   document.getElementById('stat-chart').innerHTML = string;
@@ -173,13 +207,13 @@ const funcFilterAndSort = () => {
         newChampionsArr = lol.filterChampionsMana(newChampionsArr, minNumber.value, maxNumber.value);
         statOfChamps(newChampionsArr);
         printCardsOfChampions(newChampionsArr);
-        statChart(newChampionsArr);
+        // statChart(newChampionsArr);
       }
       newChampionsArr = lol.sortChampionsCards(sortChampions.value, newChampionsArr);
       newChampionsArr = lol.filterChampionsMana(newChampionsArr, minNumber.value, maxNumber.value);
       statOfChamps(newChampionsArr);
       printCardsOfChampions(newChampionsArr);
-      statChart(newChampionsArr);
+      // statChart(newChampionsArr);
     });
 };
 
@@ -208,15 +242,15 @@ btnHome.addEventListener('click', () => {
   welcomePage.classList.remove('hide');
   tutPage.classList.remove('hide');
   championsPage.classList.add('hide');
-  infoChampion.classList.add('hide');
+  infoChampionPage.classList.add('hide');
   statsPage.classList.add('hide')
 });
 
-btnStats.addEventListener('click', ()=>{
+btnStats.addEventListener('click', () => {
   welcomePage.classList.add('hide');
   tutPage.classList.add('hide');
   championsPage.classList.add('hide');
-  infoChampion.classList.add('hide');
+  infoChampionPage.classList.add('hide');
   statsPage.classList.remove('hide')
 })
 const funcHideAndShow = () => {
@@ -225,8 +259,9 @@ const funcHideAndShow = () => {
   statsPage.classList.add('hide')
   championsPage.classList.remove('hide');
   championsListElement.classList.remove('hide');
-  infoChampion.classList.add('hide');
+  infoChampionPage.classList.add('hide');
   navFilterAndSearch.classList.remove('hide');
+  asideStats.classList.add('hide');
   sortChampions.value = 'az';
   selectRoles.value = 'default';
   minNumber.value = 0;
